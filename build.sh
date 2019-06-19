67b45e3ae8cb2ea5507960ec42c3d071090afbd0
oujiaming2018 <438661727@qq.com>
2019-06-19 19:07:40 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+echo "# spring-cloud-security-oauth2-jdbc" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:jiaming2018/spring-cloud-security-oauth2-jdbc.git
+git push -u origin master
